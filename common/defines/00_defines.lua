01f1021794191ec2381d68cbab19198a62854d6c
yhynoo <50767990+yhynoo@users.noreply.github.com>
2025-09-25 10:13:04 +0200

    ai improvements

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2515,9 +2515,9 @@ NAI = {
 
 	WANTED_UNITS_MAX_WANTED_CAP = 500,	-- Maximum wanted divisions for a country. This can be exceeded by certain hardcoded multipliers, but not by base calculation logic.
 
-	WANTED_LAND_PLANES_PER_BASE_CAPACITY_FACTOR = 1,	-- Scales how many land-based planes the AI want per air base space (excluding carriers).
+	WANTED_LAND_PLANES_PER_BASE_CAPACITY_FACTOR = 10,	-- [MOD; vanilla: 1] Scales how many land-based planes the AI want per air base space (excluding carriers).
 	WANTED_LAND_PLANES_PER_DIVISION = 20,				-- How many land-based planes the AI want for each division it wants.
-	WANTED_LAND_PLANES_TOTAL_MAX_PER_DIVISION = 100,	-- The max total number of land-based planes the AI want.
+	WANTED_LAND_PLANES_TOTAL_MAX_PER_DIVISION = 200,	-- [MOD; vanilla: 100] The max total number of land-based planes the AI want.
 
 	WANTED_CARRIER_PLANES_PER_CARRIER_CAPACITY_FACTOR = 1.5,					-- Scales how many carrier planes the AI want per carrier deck space.
 	WANTED_CARRIER_PLANES_PER_CARRIER_CAPACITY_IN_PRODUCTION_FACTOR = 1,	-- Scales how many carrier planes the AI want per deck space of carriers in production.
